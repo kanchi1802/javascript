9bea0fb3048ea5e10600911809bfd171189b7739
kanchi-gupta <175526049+kanchi1802@users.noreply.github.com>
2025-09-11 06:55:25 +0000

    datatypes and variables learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const name = "kanchi"
+let age = "19"
+var marks = "24"
+phoneNumber = 12345
+let address;
+
+/* prefer not to use var because of issue in block scope and functional scope */
+
+console.log([name, age, marks, phoneNumber, address])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+let name = "kanchi"  //string datatype
+let age = 20        //integer datatype
+let isLoggedIn = false   //boolean datatype
+
+/*  
+
+number => 2 to power 53
+bigint
+string => ""
+boolean => true or false
+null (stand alone value)
+undefined (value is not assigned)
+symbol (uniqueness ke liye)
+
+*/
+
+//object
+console.log(typeof "kanchi")
+console.log(typeof 20)
+console.log(typeof null)
